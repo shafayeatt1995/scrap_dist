381a5463e06799a9e982794ade6ee27eb1f1f9a0
shafayeatt1995 <shafayetalanik@gmail.com>
2024-08-19 17:20:09 +0600

    init

---
+++ _nuxt/static/1724061018/doctor/dr-amanullah-bin-siddiq/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1724061018",layout:"default",error:a,state:{baseUrl:"http:\u002F\u002Flocalhost:8080",windowWidth:0},serverRendered:true,routePath:"\u002Fdoctor\u002Fdr-amanullah-bin-siddiq",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1724061018/scrap/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/scrap", {data:[{_img:{}}],fetch:{},mutations:[]});
