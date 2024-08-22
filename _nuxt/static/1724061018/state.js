9269f39ad8c8f00a6e3829971067da71c8fbbc72
shafayeatt1995 <shafayetalanik@gmail.com>
2024-08-22 23:08:22 +0600

    Generated 22-08-24

--- _nuxt/static/1724061018/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1724061018",layout:"default",error:a,state:{baseUrl:"http:\u002F\u002Flocalhost:8080",windowWidth:0},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
